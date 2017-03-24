e6e9437452165d42cd008031585d4ca4c6c964f6
DragostinH <dragostin.hristov@gmail.com>
2017-03-24 20:56:12 +0000

    Firing FX
    Added a firing sound effect for the computing student.

--- Assets/Scripts/NewTutorialCanvasScript.cs
+++ Assets/Scripts/NewTutorialCanvasScript.cs
@@ -15,12 +15,15 @@ public class NewTutorialCanvasScript : MonoBehaviour {
 	}
 
     public void ContinueButtonClick() {
+		
         if (gameObject.activeInHierarchy)
         {
             //gameObject.transform.parent.
             gameObject.SetActive(false);
+			gameObject.GetComponent<AudioSource> ().Play ();
             GameObject spawnPoint = GameObject.FindGameObjectWithTag("SpawnPoint");
             spawnPoint.GetComponent<SpawnEnemy>().waveInProgress = true;
+
             //Time.timeScale = 0;
         }
         else
